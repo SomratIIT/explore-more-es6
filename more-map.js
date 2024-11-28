40f1d5258bea94a819cd301eca01ba94da6e50d0
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-28 13:44:01 +0600

    More about map

--- more-map.js
+++ more-map.js
@@ -3,3 +3,16 @@ const numbers =[10,20,30];
 const result = numbers.map(n=>n*n);
 
 console.log(result);
+
+const half = numbers.map(n=> n/2);
+console.log(half);
+// More Map functionality
+const friends = ['Tom','John','Micheal','Oliver'];
+
+const lengths = friends
+.map(frnd =>frnd.length);
+
+console.log(lengths);
+
+const firstLetter = friends.map(friend=>friend[0]);
+console.log(firstLetter);
