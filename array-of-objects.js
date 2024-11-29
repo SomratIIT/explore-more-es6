5f5cb0663092dd43badedbfca6d05c6dbe78b1ba
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-29 16:15:11 +0600

    Array of objects

---
+++ array-of-objects.js
@@ -0,0 +1,31 @@
+const products = [
+
+{
+    id:1,name:'lenovo',price:65000
+},
+{
+    id:2,name:'dell',price:45000
+}
+,
+{
+    id:3,name:'hp',price:40000
+},
+{
+    id:4,name:'mac',price:150000
+}
+
+
+]
+
+// Map
+const output5 = products.map(product=>product.name );
+console.log(output5);
+
+// forEach
+
+const output6 = products.forEach(product=> console.log(product.id));
+// filter
+
+const output7 = products.filter(prod=> prod.price>100000);
+console.log(...output7);
+
