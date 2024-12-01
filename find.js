489af3f22cdeee6b3ccae5676e19997600a0dbff
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-12-01 11:13:40 +0600

    Done with es6

--- find.js
+++ find.js
@@ -1,4 +1,8 @@
-const players = [20,99,66,44,33,22];
+// const players = [20,99,66,44,33,22];
 
-const output4 = players.find(p=> p>80);
-console.log(output4);
+// const output4 = players.find(p=> p>80);
+// console.log(output4);
+function min(nums) { return Math.min(nums) }
+console.log(min( [1,3,2 ]));
+const [a, b] = [1,2,3,4,45,5]; 
+console.log(a+b);
--- this.js
+++ this.js
@@ -1,13 +1,16 @@
-class Person{
-constructor(name,age)
-{
-    this.name = name;
-    this.age= age;
-}
-sleep(){
-    console.log(`${this.name} Ghumese Poreche`);
-}
+// class Person{
+// constructor(name,age)
+// {
+//     this.name = name;
+//     this.age= age;
+// }
+// sleep(){
+//     console.log(`${this.name} Ghumese Poreche`);
+// }
  
-}
-const kodom= new Person('Kodom Ali',33);
-kodom.sleep();
+// }
+// const kodom= new Person('Kodom Ali',33);
+// kodom.sleep();
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2==1);
+console.log(output);
